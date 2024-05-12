3c3ea0d1b33fb0d0c91916ba35c74568ff2d0c58
Karim <theluckiestloser@inbox.ru>
2024-05-12 19:12:39 +0300

    здарова ептыть

---
+++ items_dir.py
@@ -0,0 +1,22 @@
+# class Shop:
+#     def __init__(self, weapon, armor, potions):
+#         self.weapon = weapon
+#         self.armor = armor
+#         self.potions = potions
+# class Weapons(Shop):
+#     def bruh()
+
+# # class Armor:
+# #     def __init__(self, helmet, plate, boots):
+# #         self.helmet = helmet
+# #         self.plate = plate
+# #         self.boots = boots
+    
+# # class Potions:
+# #     def __init__(self, health, strength, speed):
+# #         self.health = health
+# #         self.strength = strength
+# #         self.speed = speed
+
+# 1-ый критерий урон/резисты/значения, 2-ой критерий цена
+items = {'Iron_Sword': [0, 0], 'Iron_Axe': [0, 0], 'Golden_Sword': [0, 0], 'Golden_Axe': [0, 0], 'Meteor_Sword': [0, 0], 'Meteor_Axe': [0, 0], 'Ripped_Helmet': [0, 0], 'Ripped_Chestplate': [0, 0], 'Ripped_Boots': [0, 0], 'Iron_Helmet': [0, 0], 'Iron_Chestplate': [0, 0], 'Iron_Boots': [0, 0], 'Bone_Helmet': [0, 0], 'Bone_Chestplate': [0, 0], 'Bone_Boots': [0, 0], 'Health_Potion_1': [0, 0], 'Health_Potion_2': [0, 0], 'Health_Potion_3': [0, 0], 'Strength_Potion_1': [0, 0], 'Strength_Potion_2': [0, 0], 'Strength_Potion_3': [0, 0], 'Luck_Potion_1': [0, 0], 'Luck_Potion_2': [0, 0], 'Luck_Potion_3': [0, 0]}
